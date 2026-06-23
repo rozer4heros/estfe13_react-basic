82067ea403a607fa2dee814889ccfd8325aa5886
rozer4heros <rozer4heros@gmail.com>
2026-06-23 14:26:21 +0900

    feat: Welcome Mode

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import Nav from "./components/Nav.jsx";
 import MyArticle from "./components/MyArticle.jsx";
 
 function App() {
+  const [mode, setMode] = useState("welcome");
   const [subject, setSubject] = useState({
     title: "프론트엔드 개발자",
     desc: "기본언어인 html, css, javascript부터 학습합니다.",
   });
-
   const [content, setContent] = useState([
     // { id: 0, title: "HTML", desc: "HyperText Markup Language" },
     { id: 1, title: "UI/UX 개발", desc: "사용자 경험을 고려한 직관적이고 반응성 높은 화면 구현" },
@@ -17,14 +17,21 @@ function App() {
     { id: 3, title: "애니메이션 구현", desc: "상태 변화에 따른 자연스럽고 동적인 화면 효과 구현" },
   ]);
 
-  const contentArticleEls = content.map((c) => <MyArticle key={c.id} title={c.title} desc={c.desc} />);
+  const welcome = { title: "welcome", desc: "Welcome to react" };
+  // const contentArticleEls = content.map((c) => <MyArticle key={c.id} title={c.title} desc={c.desc} />);
+  let _title = null;
+  let _desc = null;
+
+  if (mode === "welcome") {
+    _title = welcome.title;
+    _desc = welcome.desc;
+  }
 
   return (
     <>
       <MyHeader title={subject.title} desc={subject.desc} />
       <Nav data={content} />
-      <MyArticle title={content[0].title} desc={content[0].desc} />
-      {/* {contentArticleEls} */}
+      <MyArticle title={_title} desc={_desc} />
     </>
   );
 }
