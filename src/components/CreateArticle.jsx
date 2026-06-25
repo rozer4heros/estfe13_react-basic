3edbc82f91d860e2ed8e1d667adc7f04134c2848
rozer4heros <rozer4heros@gmail.com>
2026-06-25 09:27:23 +0900

    refactor: ArticleForm 컴포넌트 적용

--- src/components/CreateArticle.jsx
+++ src/components/CreateArticle.jsx
@@ -1,34 +1,12 @@
+import ArticleForm from "./ArticleForm";
+
 function CreateArticle({ onSubmit }) {
   console.log("CreateArticle render");
 
   return (
     <>
       <h2>Create Article</h2>
-      <form
-        action=""
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (!e.target.title.value) {
-            alert("제목이 필요합니다!");
-          } else {
-            onSubmit(e.target.title.value, e.target.desc.value, e.target.level.value);
-          }
-        }}
-      >
-        <div>
-          <label htmlFor="title">제목: </label>
-          <input type="text" name="title" id="title" />
-        </div>
-        <div>
-          <label htmlFor="desc">설명: </label>
-          <textarea name="desc" id="desc"></textarea>
-        </div>
-        <div>
-          <label htmlFor="level">난이도: </label>
-          <input type="text" name="level" id="level" />
-        </div>
-        <button>생성</button>
-      </form>
+      <ArticleForm onSubmit={onSubmit} />
     </>
   );
 }
