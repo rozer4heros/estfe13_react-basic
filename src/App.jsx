5e52461575955707cb987f36e25360c56791f60e
rozer4heros <rozer4heros@gmail.com>
2026-06-25 09:53:25 +0900

    modify: 삭제를 취소하면 welcome mode로 돌아가지 않음

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,12 @@ function App() {
     desc: "기본언어인 html, css, javascript부터 학습합니다.",
   });
   const [content, setContent] = useState([
-    { id: "1", title: "UI/UX 개발", desc: "사용자 경험을 고려한 직관적이고 반응성 높은 화면 구현", level: "2" },
+    { id: "1", title: "UI/UX 개발", desc: "사용자 경험을 고려한 직관적이고 반응성 높은 화면 구현", level: "4" },
     {
       id: "2",
       title: "재사용이 가능한 UI 개발",
       desc: "컴포넌트 기반으로 동일한 UI를 효율적으로 재사용 가능",
-      level: "1",
+      level: "2",
     },
     { id: "3", title: "애니메이션 구현", desc: "상태 변화에 따른 자연스럽고 동적인 화면 효과 구현", level: "3" },
   ]);
@@ -47,12 +47,12 @@ function App() {
   const handleChangeModeCreate = useCallback(() => {
     setMode("create");
   }, []);
-  const handleDelete = useCallback(() => {
+  const handleDelete = () => {
     if (window.confirm("정말 삭제할까요?")) {
       setContent((prev) => prev.filter((item) => item.id !== id));
+      setMode("welcome");
     }
-    setMode("welcome");
-  }, [id]);
+  };
 
   if (mode === "welcome") {
     _title = welcome.title;
